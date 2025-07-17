796c7f3b7f3b2b283794507440af74ffdabc2898
Rui Campos <ruimiguelfcampos@gmail.com>
2025-07-17 15:56:54 +0100

    02_churn_by_contract.sql

---
+++ sql/02_churn_by_contract.sql
@@ -0,0 +1,11 @@
+SELECT 
+  Contract,
+  COUNT(*) AS total_customers,
+  SUM(CASE WHEN Churn = TRUE THEN 1 ELSE 0 END) AS churned_customers,
+  ROUND(SUM(CASE WHEN Churn = TRUE THEN 1 ELSE 0 END) * 1.0 / COUNT(*), 4) AS churn_rate
+FROM 
+  `stone-poetry-462613-q7.Telco_Customer_Churn.1`
+GROUP BY 
+  Contract
+ORDER BY 
+  churn_rate DESC;
